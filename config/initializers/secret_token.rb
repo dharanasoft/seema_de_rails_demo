d60034271087375782e52462022ef52daca52fec
dhirengupta <dhirengupta@gmail.com>
2011-09-29 18:09:56 +0530

    added address model and controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeemaDeRailsDemo::Application.config.secret_token = '8e095a21d10cbef4123e5c552414fa1cce11b3a90ada4b4e88419fb9804957e341565d6f88f3362583c7b5f9548107360182649d9853e29a70054956aea60825'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SeemaDeRailsDemo::Application.config.session_store :cookie_store, :key => '_seema_de_rails_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SeemaDeRailsDemo::Application.config.session_store :active_record_store
